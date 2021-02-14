22725302b01c848b487002ab78cc838946388e41
kls_k <vilau_claudiu@yahoo.com>
2021-02-14 15:42:03 +0100

    update tests

--- src/test/java/com/openclassrooms/safetynetalerts/integration/EndPointsControlleriTest.java
+++ src/test/java/com/openclassrooms/safetynetalerts/integration/EndPointsControlleriTest.java
@@ -1,14 +1,12 @@
 package com.openclassrooms.safetynetalerts.integration;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import java.io.IOException;
-import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,8 +16,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-import com.openclassrooms.safetynetalerts.dao.JsonDaoImplements;
-import com.openclassrooms.safetynetalerts.model.PhoneAlert;
+import com.google.inject.matcher.Matchers;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -131,4 +128,103 @@ public class EndPointsControlleriTest {
 		mockMvc.perform(get("/phoneAlert").param("firestation", param_station)).andExpect(status().isOk()).equals(null);
 	}
 
+	@Test
+	public void testFireAddress() throws Exception {
+
+		/*
+		 * L'utilisateur accède à l’URL :
+		 * 
+		 * http://localhost:9090/fire?address=<address>
+		 *
+		 * Le système retourne une liste des habitants vivants à l’adresse donnée ainsi
+		 * que le numéro de la caserne de pompiers la desservant. La liste doit inclure
+		 * : le nom, le numéro de téléphone, l’âge et les antécédents médicaux
+		 * (médicaments, posologie et allergies) de chaque personne.
+		 */
+
+		String param_address = "1509 Culver St";
+		mockMvc.perform(get("/fire").param("address", param_address)).andExpect(status().isOk()).andExpect(jsonPath("$[0].lastName", is("Boyd")));
+
+	}
+
+	@Test
+	public void testFireAddressNoAddress() throws Exception {
+
+		/*
+		 * L'utilisateur accède à l’URL :
+		 * 
+		 * http://localhost:9090/fire?address=<address>
+		 *
+		 * Le système retourne une liste des habitants vivants à l’adresse donnée ainsi
+		 * que le numéro de la caserne de pompiers la desservant. La liste doit inclure
+		 * : le nom, le numéro de téléphone, l’âge et les antécédents médicaux
+		 * (médicaments, posologie et allergies) de chaque personne.
+		 */
+
+		String param_address = "";
+		mockMvc.perform(get("/fire").param("address", param_address)).andExpect(status().isOk()).equals(null);
+	}
+
+	@Test
+	public void testStationListFirestation() throws Exception {
+
+		/*
+		 * L'utilisateur accède à l’URL :
+		 * 
+		 * http://localhost:9090/flood/station?station=<a list of station_numbers>
+		 * 
+		 * Le système retourne une liste de tous les foyers desservis par la caserne.
+		 * Cette liste doit regrouper les personnes par adresse. La liste doit inclure :
+		 * le nom, le numéro de téléphone et l’âge des habitants et faire figurer les
+		 * antécédents médicaux (médicaments, posologie et allergies) à côté de chaque
+		 * nom.
+		 * 
+		 */
+
+		List<String> param_station = new ArrayList<>();
+		param_station.add("1");
+		param_station.add("3");
+		mockMvc.perform(get("/flood/station").param("station", param_station.get(0).toString()).param("station", param_station.get(1).toString())).andExpect(status().isOk())
+					.andExpect(jsonPath("$[0].address", containsString("644"))).andExpect(jsonPath("$[1].address", containsString("908 73rd St")))
+					.andExpect(jsonPath("$[*].address").isArray());	
+	}
+
+	@Test
+	public void testPersonInfo() throws Exception {
+
+		/*
+		 * L'utilisateur accède à l’URL :
+		 * 
+		 * http://localhost:9090/personInfo?firstName=<firstName>&lastName=<lastName>
+		 * 
+		 * Le système retourne le nom, l’adresse, l’âge, l’adresse mail et les
+		 * antécédents médicaux (médicaments, posologie et allergies) de chaque
+		 * habitant. Si plusieurs personnes portent le même nom, elles doivent toutes
+		 * apparaître.
+		 * 
+		 */
+
+		String param_firstName = "John";
+		String param_lastName = "Boyd";
+		mockMvc.perform(get("/personInfo").param("firstName", param_firstName).param("lastName", param_lastName)).andExpect(status().isOk()).andExpect(jsonPath("$[0].lastName", is("Boyd")));
+	}
+
+	@Test
+	public void testCommunityEmail() throws Exception {
+
+		/*
+		 * L'utilisateur accède à l’URL :
+		 * 
+		 * http://localhost:9090/communityEmail?city=<city>
+		 * 
+		 * Le système retourne les adresses mail de tous les habitants de la ville.
+		 * 
+		 */
+
+		String param_city = "Culver";
+		mockMvc.perform(get("/communityEmail").param("city", param_city)).andExpect(status().isOk()).andExpect(jsonPath("$[0].listEmails", is(notNullValue())));
+
+	}
+
+	
 }
