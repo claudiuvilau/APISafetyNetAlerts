4d8ea6bc35f6802d74c00405413b5d9e3bd226c6
Claudiu Vilau <vilau_claudiu@yahoo.com>
2021-10-11 22:15:04 +0200

    test one commit

--- src/main/java/com/openclassrooms/safetynetalerts/controller/EndPointsController.java
+++ src/main/java/com/openclassrooms/safetynetalerts/controller/EndPointsController.java
@@ -1,5 +1,6 @@
 package com.openclassrooms.safetynetalerts.controller;
 
+
 import org.json.simple.JSONArray;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
